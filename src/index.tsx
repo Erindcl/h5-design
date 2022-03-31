92646a800a68fe21a34ca1faba2081b8dce4af8c
dengcl <dengcl@253.com>
2022-03-31 09:52:26 +0800

    feat: 添加 antd

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import Router from './router';
 import { Provider } from 'react-redux';
 import store from './store';
+import { ConfigProvider } from 'antd';
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import zhCN from 'antd/lib/locale/zh_CN';
+import './styles/theme.less';
 import './styles/index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router />
+    <ConfigProvider locale={zhCN}>
+      <Router />
+    </ConfigProvider>
   </Provider>,
   document.getElementById('root')
 );
