04f0f67b29f36d5a7bec3483abe61d232d13baaf
dengcl <dengcl@253.com>
2022-04-15 10:53:49 +0800

    feat: 修改 我的作品 页面

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -18,7 +18,14 @@ const Page: React.FC = () => {
     pageIndex: 1,
     pageSize: 10
   });
-  const [h5List, setH5List] = useState([]);
+  const [h5List, setH5List] = useState([
+    { id: 1 },
+    { id: 1 },
+    { id: 1 },
+    { id: 1 },
+    { id: 1 },
+    { id: 1 },
+  ]);
 
   const handleToDesignPage = (record?: any) => {
     navigate('/design', { state: record || null })
@@ -50,17 +57,24 @@ const Page: React.FC = () => {
         <div className="home-page-main">
           <div className="page-title">我的作品</div>
           {h5List.length > 0 && <div className="h5-list">
-
+            {h5List.map((ele, index) => (<div key={index} className="h5-item">
+              <div className="h5-cover">
+              </div>
+              <div className="h5-info">
+                <div className="h5-name">叮咚营销页面</div>
+                <div className="h5-date">2022-04-12 12:22:33</div>
+              </div>
+            </div>))}
           </div>}
           {h5List.length === 0 && <Empty description="暂无作品，赶快去添加吧！" image={Empty.PRESENTED_IMAGE_SIMPLE} />}
-          <div className="pagination-wrapper">
+          {h5List.length !== 0 && <div className="pagination-wrapper">
             <Pagination
               showSizeChanger
               onChange={handlePaginationChange}
               current={pageInfo.pageIndex}
               total={pageInfo.total}
             />
-          </div>
+          </div>}
         </div>
       </div>
     </div>
